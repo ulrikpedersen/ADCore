ff6f5c85499e66de056a71913ee0d5870bd1239d
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-07-29 09:59:35 +0100

    updated CI to use travis containers and build-matrix

--- ci/build-synapps-deps.sh
+++ ci/build-synapps-deps.sh
@@ -1,40 +1,119 @@
 #!/bin/bash
 
-mkdir external
-cd external
-wget -nv http://www.aps.anl.gov/bcda/synApps/tar/sscan_R2-10.tar.gz
-tar -zxf sscan_R2-10.tar.gz
-echo "EPICS_BASE=/usr/lib/epics" > sscan-2-10/configure/RELEASE
-make -C sscan-2-10/
+ADCORE_DIR=`pwd`
+mkdir -p $HOME/epics
+cd $HOME/epics
 
-wget -nv http://www.aps.anl.gov/bcda/synApps/tar/calc_R3-4-2.tar.gz
-tar -zxf calc_R3-4-2.tar.gz
-echo "SSCAN=`pwd`/sscan-2-10" > calc-3-4-2/configure/RELEASE
-echo "EPICS_BASE=/usr/lib/epics" >> calc-3-4-2/configure/RELEASE
-make -C calc-3-4-2/
+#export EPICS_BASE=$HOME/epics/base-3.14.12.5
+#export EPICS_HOST_ARCH=linux-x86_64
 
-wget -nv http://www.aps.anl.gov/epics/download/modules/asyn4-26.tar.gz
-tar -zxf asyn4-26.tar.gz
-echo "EPICS_BASE=/usr/lib/epics" > asyn4-26/configure/RELEASE
-#echo "EPICS_LIBCOM_ONLY=YES" >> asyn4-26/configure/CONFIG_SITE
-make -C asyn4-26/
+if [ ! -f baseR3.14.12.5.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/epics/download/base/baseR3.14.12.5.tar.gz
+  tar -zxf baseR3.14.12.5.tar.gz
+fi
+if [ ! -d "$HOME/epics/base-3.14.12.5/lib/$EPICS_HOST_ARCH" ]; then
+  make -C base-3.14.12.5
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build base 3.14.12.5 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory epics base';
+fi
+
+if [ ! -f sscan_R2-10.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/bcda/synApps/tar/sscan_R2-10.tar.gz
+  tar -zxf sscan_R2-10.tar.gz
+fi
+if [ ! -d "$HOME/epics/sscan-2-10/lib/$EPICS_HOST_ARCH" ]; then
+  echo "EPICS_BASE=$EPICS_BASE" > sscan-2-10/configure/RELEASE
+  make -C sscan-2-10/
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build sscan 2-10 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory sscan';
+fi
+
+if [ ! -f calc_R3-4-2.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/bcda/synApps/tar/calc_R3-4-2.tar.gz
+  tar -zxf calc_R3-4-2.tar.gz
+fi
+if [ ! -d "$HOME/epics/calc-3-4-2/lib/$EPICS_HOST_ARCH" ]; then
+  echo "SSCAN=`pwd`/sscan-2-10" > calc-3-4-2/configure/RELEASE
+  echo "EPICS_BASE=$EPICS_BASE" >> calc-3-4-2/configure/RELEASE
+  make -C calc-3-4-2/
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build calc 3-4-2 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory calc';
+fi
+
+if [ ! -f asyn4-26.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/epics/download/modules/asyn4-26.tar.gz
+  tar -zxf asyn4-26.tar.gz
+fi
+if [ ! -d "$HOME/epics/asyn4-26/lib/$EPICS_HOST_ARCH" ]; then
+  echo "EPICS_BASE=$EPICS_BASE" > asyn4-26/configure/RELEASE
+  #echo "EPICS_LIBCOM_ONLY=YES" >> asyn4-26/configure/CONFIG_SITE
+  make -C asyn4-26/
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build asyn 4-26 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory asyn';
+fi
 
 
-wget -nv http://www.aps.anl.gov/bcda/synApps/tar/busy_R1-6-1.tar.gz
-tar -zxf busy_R1-6-1.tar.gz
-echo "EPICS_BASE=/usr/lib/epics" > busy-1-6-1/configure/RELEASE
-echo "ASYN=`pwd`/asyn4-26" >> busy-1-6-1/configure/RELEASE
-make -C busy-1-6-1/
+if [ ! -f busy_R1-6-1.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/bcda/synApps/tar/busy_R1-6-1.tar.gz
+  tar -zxf busy_R1-6-1.tar.gz
+fi
+if [ ! -d "$HOME/epics/busy-1-6-1/lib/$EPICS_HOST_ARCH" ]; then
+  echo "EPICS_BASE=$EPICS_BASE" > busy-1-6-1/configure/RELEASE
+  echo "ASYN=`pwd`/asyn4-26" >> busy-1-6-1/configure/RELEASE
+  make -C busy-1-6-1/
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build busy 1-6-1 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory busy';
+fi
 
-wget -nv http://www.aps.anl.gov/bcda/synApps/tar/autosave_R5-5.tar.gz
-tar -zxf autosave_R5-5.tar.gz
-echo "EPICS_BASE=/usr/lib/epics" > autosave-5-5/configure/RELEASE
-make -C autosave-5-5/
+if [ ! -f autosave_R5-5.tar.gz ]; then
+  wget -nv http://www.aps.anl.gov/bcda/synApps/tar/autosave_R5-5.tar.gz
+  tar -zxf autosave_R5-5.tar.gz
+fi
+if [ ! -d "$HOME/epics/autosave-5-5/lib/$EPICS_HOST_ARCH" ]; then
+  echo "EPICS_BASE=$EPICS_BASE" > autosave-5-5/configure/RELEASE
+  make -C autosave-5-5/
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build autosave 5-5 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory autosave';
+fi
 
-wget -nv https://github.com/epics-modules/iocStats/archive/3.1.14.tar.gz
-tar -zxf 3.1.14.tar.gz
-echo "EPICS_BASE=/usr/lib/epics" > iocStats-3.1.14/configure/RELEASE
-make -C iocStats-3.1.14
+if [ ! -f 3.1.14.tar.gz ]; then
+  wget -nv https://github.com/epics-modules/iocStats/archive/3.1.14.tar.gz
+  tar -zxf 3.1.14.tar.gz
+fi
+if [ ! -d "$HOME/epics/iocStats-3.1.14/lib/$EPICS_HOST_ARCH" ]; then
+  echo "EPICS_BASE=$EPICS_BASE" > iocStats-3.1.14/configure/RELEASE
+  make -C iocStats-3.1.14
+  if [ $? -ne 0 ]; then 
+    echo "Failed to build iocStats 3.1.14 for $EPICS_HOST_ARCH"
+    exit 2
+  fi
+else
+  echo 'Using cached directory iocStats';
+fi
 
-cd ..
+cd $ADCORE_DIR
 
--- ci/configure.sh
+++ ci/configure.sh
@@ -3,7 +3,7 @@
 
 # Generate the configure/RELEASE.local and configure/CONFIG_SITE.linux-x86_64.Common
 # with the details of where to find various external libraries.
-echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
+echo "EPICS_BASE=$EPICS_BASE"                >> configure/RELEASE.local
 echo "HDF5=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HDF5_LIB=/usr/lib"                     >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
@@ -15,12 +15,12 @@ echo "HOST_OPT=NO"                           >> configure/CONFIG_SITE.linux-x86_
 echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
 
-echo "ASYN=`pwd`/external/asyn4-26"          >> configure/RELEASE.local
-echo "BUSY=`pwd`/external/busy-1-6-1"        >> configure/RELEASE.local
-echo "CALC=`pwd`/external/calc-3-4-2"        >> configure/RELEASE.local
-echo "SSCAN=`pwd`/external/sscan-2-10"       >> configure/RELEASE.local
-echo "AUTOSAVE=`pwd`/external/autosave-5-5"  >> configure/RELEASE.local
-echo "DEVIOCSTATS=`pwd`/external/iocStats-3.1.14"  >> configure/RELEASE.local
+echo "ASYN=$HOME/epics/asyn4-26"          >> configure/RELEASE.local
+echo "BUSY=$HOME/epics/busy-1-6-1"        >> configure/RELEASE.local
+echo "CALC=$HOME/epics/calc-3-4-2"        >> configure/RELEASE.local
+echo "SSCAN=$HOME/epics/sscan-2-10"       >> configure/RELEASE.local
+echo "AUTOSAVE=$HOME/epics/autosave-5-5"  >> configure/RELEASE.local
+echo "DEVIOCSTATS=$HOME/epics/iocStats-3.1.14"  >> configure/RELEASE.local
 
 # Configure the example IOCs in preparation for build
 echo "BUILD_IOCS=YES"                        >> configure/CONFIG_SITE.linux-x86_64.Common
